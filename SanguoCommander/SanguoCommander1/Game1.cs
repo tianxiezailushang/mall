18c82516231fa579f8127c83347bbf27b4bc20d5
OpenXLive <Henry@openxlive.com>
2012-09-11 20:45:20 +0800

    The Sample code in session1
    Comment language:Chinese

---
+++ SanguoCommander/SanguoCommander1/Game1.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using cocos2d;
+
+namespace SanguoCommander
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            this.graphics.IsFullScreen = true;
+
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+
+            InactiveSleepTime = TimeSpan.FromSeconds(1);
+
+            CCApplication application = new AppDelegate(this, graphics);
+            this.Components.Add(application);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            base.Update(gameTime);
+        }
+    }
+}
